4f9225af0581ecbd43b0c335030b9634ab8f21ff
Jacky Xu <jackyxu07@gmail.com>
2019-09-22 15:45:09 -0400

    fix path issue

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,33 @@
 import createDefaultConfig from '@open-wc/building-rollup/modern-config';
+import copy from 'rollup-plugin-copy';
+
+const path = require('path');
 
 // if you need to support IE11 use "modern-and-legacy-config" instead.
 // import createDefaultConfig from '@open-wc/building-rollup/modern-and-legacy-config';
 
-export default createDefaultConfig({
+const outputDir = 'docs';
+
+// list of global scripts in node_modules to be loaded in index.html
+// relative path has to be preserved
+const globalScripts = [
+  'node_modules/graphql.js/graphql.min.js',
+];
+
+const defaultConfig = createDefaultConfig({
   input: './index.html',
-  outputDir: 'docs',
+  outputDir,
 });
+
+export default {
+  ...defaultConfig,
+  plugins: [
+    copy({
+      targets: globalScripts.map(glob => ({
+        src: glob,
+        dest: `${outputDir}/${path.dirname(glob)}`,
+      })),
+    }),
+    ...defaultConfig.plugins,
+  ],
+};
